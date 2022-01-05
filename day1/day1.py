986c2947f34b6c48d92af80c1d45ae955fc8b152
vikas23737 <vr3953282@gmail.com>
2022-01-05 13:17:16 +0530

    Create day1.py

---
+++ day1/day1.py
@@ -0,0 +1,15 @@
+#1st program to add two num 
+a=int( input("enter first number is:-"))
+b= int(input("enter second number is:-"))
+print("sum:-",a+b)
+
+# 2nd program to add two string
+m = str(input("enter name:-"))
+n= str(input("enter surname:-"))
+print ("full name:-",m+n)
+
+#3rd program to add two float
+r = float(input("enter number:-"))
+s= float(input("enter number:-"))
+print ("total of number:-",r+s)
+
