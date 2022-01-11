59cfa87a6ccb9c631ef3bf266e4da8fea5a54aae
vikas23737 <96410827+vikas23737@users.noreply.github.com>
2022-01-11 18:59:45 +0530

    Create calculating modulus,exponent ,and floor division and use comparison operators.py

---
+++ day3/calculating modulus,exponent ,and floor division and use comparison operators.py
@@ -0,0 +1,16 @@
+#1st program for calculating modulus, exponent , and floor division
+a=int(input("Enter first number:"))
+b=int(input("Enter second number :"))
+print(f"modulus of {a} and {b} is {a%b}")
+print(f" Exponent of {a} and {b} is {a**b}")
+print(f" Floor Division of {a} and {b} is {a//b}")
+
+#2nd program that use comparison operator
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print(f"Is a equal to b (True : False): {a==b} ")
+print(f"Is a  not equal to b (True :  False): {a!=b} ")
+print(f"Is a is greater than b (True :  False): {a>b} ")
+print(f"Is a is less than b (True :  False): {a<b} ")
+print(f"Is a is greater than equal to b (True :  False): {a>=b} ")
+print(f"Is a is less than equal to b (True :  False): {a<=b} ")
